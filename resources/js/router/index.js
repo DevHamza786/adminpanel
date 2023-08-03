9eb8541843ecc0d0fdf67683f79f275a2b36a58f
DevHamza786 <muhammadhamzamehmood@gmail.com>
2023-08-03 19:22:51 +0000

    Live chaning

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,11 +1,11 @@
+import { canNavigate } from '@layouts/plugins/casl'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router'
-import { isUserLoggedIn } from './utils'
 import routes from '~pages'
-import { canNavigate } from '@layouts/plugins/casl'
+import { isUserLoggedIn } from './utils'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     // ℹ️ We are redirecting to different pages based on role.
     // NOTE: Role is just for UI purposes. ACL is based on abilities.
